0a1e398b2657c09e7152f47f77f0e8e1f8ba3333
hcxie20 <hcxie20@126.com>
2020-02-21 15:08:54 -0800

    basic page settings

---
+++ MHWI-master/README.md
@@ -0,0 +1,6 @@
+# PC MHWI Weapon Defense Cheats Tool CT
+
+2020.01.15 更新冰原DLC（399335）内容，新增冰原素材，新增冰原装饰品，新增家具类目，新增场景动物<br>
+2020.02.04 新增冰原武器ID
+
+<img src="https://i.imgur.com/PpGSxVy.png" alt="MHWI"/>
---
+++ MHWI/MHWI/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MHWI
+//
+//  Created by XIE haochen on 2/20/20.
+//  Copyright © 2020 XIE haochen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
